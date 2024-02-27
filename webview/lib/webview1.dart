0ec0d292cb5190a5992f5dc09074977fce28ca5e
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-02-27 16:52:37 +0600

    flutter_webview_plugin :

--- webview/lib/webview1.dart
+++ webview/lib/webview1.dart
@@ -8,14 +8,7 @@ class Webview1 extends StatefulWidget {
 class Webviewstate extends State<Webview1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Organic web"),
-        backgroundColor: Theme.of(context).colorScheme.onSecondary,
-        
-      ),
-      body: ,
-    );
+    return  WebviewScaffold()
 
   }
 }
