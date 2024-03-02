b59770001b12f0db10d9a6945ddc4ddd499f7814
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2024-03-02 16:17:25 +0600

    use plugin Slpashscrren

--- webview/lib/main.dart
+++ webview/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview/webview1.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:webview/webview2.dart';
 
 void main() {
@@ -17,58 +17,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      //not make Slpash scrren use plagin animated_splash_screen: ^1.3.0
+      home: AnimatedSplashScreen(
+        splash:Image.asset("assets/images/scanenr.gif") ,
+        //duration:5000
+        backgroundColor: Colors.amber.shade700,
+        splashTransition: SplashTransition.scaleTransition,
+        nextScreen:Webview2() ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Webview"),
-      ),
-      body: Center(
-        child: Container(
-          width: 300,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Webview1()));
-              },
-              child: Text(
-                "web1",
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Colors.amber[700],
-              padding: EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => Webview2()));
-              },
-              child: Text(
-                "web2",
-                style: TextStyle(color: Colors.white),
-              ),
-              color: Colors.amber[700],
-              padding: EdgeInsets.symmetric(horizontal: 70, vertical: 10),
-            ),
-          ]),
-        ),
-      ),
-    );
-  }
-}
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold( 
+//           appBar: AppBar(
+//             title: Text("Webview"),
+//             backgroundColor: Colors.blue,
+//           ),
+//           body: Text("hello"),
+//     );
+//   }
+// }
--- webview/lib/webview2.dart
+++ webview/lib/webview2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
 
 class Webview2 extends StatefulWidget {
   @override
@@ -9,29 +9,13 @@ class Webview2 extends StatefulWidget {
 class WebsviewState extends State<Webview2> {
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-        url: "https://nevonprojects.com/project-ideas/software-project-ideas/",
-        appBar: AppBar(
-          title: Text("Knoledage Hub"),
-          backgroundColor: Colors.blue),
-          withZoom: true,
-          withJavascript: true,
-          scrollBar: true,
-          initialChild: Center(child: Text("loading")),
-          bottomNavigationBar:Padding(
-            padding: EdgeInsets.all(14),
-            child: Text("Footer"),
-            
-            ) ,
-            persistentFooterButtons: [
-              Center(
-                child: CircleAvatar(
-                  backgroundColor: Colors.yellow,
-                  child: Icon(Icons.balance),
-                ),
-              )
-            ],
-        );
+    return Scaffold (
+      appBar: AppBar(
+            title: Text("Webview"),
+            backgroundColor: Colors.blue,
+          ),
+          body: Text("web"),
+     );
         
   }
 }
